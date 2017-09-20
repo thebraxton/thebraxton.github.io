fae0cfba284abd6f397f17f3aace8280cdabc1ed
thebraxton <31716621+thebraxton@users.noreply.github.com>
2017-09-20 15:26:18 -0500

    added gay shit for forum

---
+++ footer.php
@@ -0,0 +1,17 @@
+</div><!-- content -->
+</div><!-- wrapper -->
+<div id="footer">Created for Nettuts+</div>
+</body>
+</html>
+<?php
+$error = false;
+if($error = false)
+{
+    //the beautifully styled content, everything looks good
+    echo '<div id="content">some text</div>';
+}
+else
+{
+    //bad looking, unstyled error :-( 
+} 
+?>
